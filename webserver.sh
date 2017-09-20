72bad761b4477a370982459bf74773df8d4030bc
USuck <yoUSuck.0327@gmail.com>
2017-09-21 00:32:28 +0900

    Implemented directory listing & url decoding

--- webserver.sh
+++ webserver.sh
@@ -4,6 +4,8 @@
 PIPE='/tmp/serverpipe'
 [ -p $PIPE ] || mkfifo $PIPE
 
+IFS_BACKUP=$IFS
+
 serve() {
 	# --- Parse request ---
 	read -r REQUEST_METHOD REQUEST_URI REQUEST_HTTP_VERSION
@@ -14,14 +16,33 @@ serve() {
 		line=$(echo "$line" | tr -d '[\r\n]')
 		[ -z "$line" ] && break
 	done
-	# --- Serve content ---
-	if [ -f ".$REQUEST_URI" ]; then
+	IFS=$'\n'
+	# --- Decode URIComponent ---
+	FILE_PATH=".`echo $REQUEST_URI | nkf --url-input`"
+	# --- Serve file ---
+	if [ -f "$FILE_PATH" ]; then
 		echo "HTTP/1.0 200 OK\n"
-		cat ".$REQUEST_URI"
+		cat $FILE_PATH
+	# --- Display directory listing ---
+	elif [[ $REQUEST_URI =~ /$ ]]; then
+		echo "HTTP/1.0 200 OK\n"
+		echo "<html><head><meta charset='utf-8' /></head><body>"
+		echo $(directoryIndex $FILE_PATH)
+		echo "</body></html>"
+	# --- When the file doesn't exist ---
 	else
 		echo "HTTP/1.1 404 Not Found\n"
 		echo "404 Not Found"
 	fi
+	IFS=$IFS_BACKUP
+}
+
+directoryIndex() {
+	echo "<ul>"
+	for file in `ls -p $1`; do
+		echo "<li><a href='${file}'>${file}</a></li>"
+	done
+	echo "</ul>"
 }
 
 # --- Run process ---
